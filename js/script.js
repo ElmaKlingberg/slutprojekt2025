a9f1531982e9636473a0f3737efdabef209812ce
ElmaKlingberg <elma.klingberg@elev.ga.ntig.se>
2025-03-28 14:31:18 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
-//JAVASCRIPT FIL HÄR
+document.getElementById("toggle").addEventListener("click"), function() {var checklist = document.getElementById("checklist"); if (checklist.classList.contains("gomd")){ checklist.classList.remove("gomd");} else { checklist.classList.add("gomd");}};
+
