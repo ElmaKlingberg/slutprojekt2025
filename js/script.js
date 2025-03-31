01d56ec7fecf13121075bfb677480a7381b4ac55
ElmaKlingberg <elma.klingberg@elev.ga.ntig.se>
2025-03-31 13:07:48 +0200

    fixat min checklist

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-document.getElementById("toggle").addEventListener("click"), function() {var checklist = document.getElementById("checklist"); if (checklist.classList.contains("gomd")){ checklist.classList.remove("gomd");} else { checklist.classList.add("gomd");}};
+const toggle = document.querySelector("#toggle");
+const checklist = document.querySelector("#checklist");
+
+function toggleMenu() {
+    checklist.classList.toggle("gomd");
+}
+
+toggle.addEventListener("click",toggleMenu);
 
